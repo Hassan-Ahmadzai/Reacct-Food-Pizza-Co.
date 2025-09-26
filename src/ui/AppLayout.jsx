e52084d5369888fc5be5210dd548841dabc82ea3
Shakeb Ahmadzai <ahmadzaiahmadshakeb@gmail.com>
2025-09-26 11:10:03 +0430

    Initial Commit

---
+++ src/ui/AppLayout.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Header from "./Header";
+
+function AppLayout() {
+    return (
+        <div>
+            <Header />
+            
+            <main>
+                
+            </main>
+        </div>
+    );
+};
+
+export default AppLayout;
---
+++ src/ui/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+
+function Header() {
+    return (
+        <header>
+            <Link>Fast React Pizza Co.</Link>
+
+            <p>Jonas</p>
+        </header>
+    );
+};
+
+export default Header;
